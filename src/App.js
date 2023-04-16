597dd89414236a7c92e22307269888147e5a2565
vladfarias <vladimir.farias@avanade.com>
2023-04-16 10:42:37 -0300

    feat: created internal routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {Switch, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import Disciplinas from './Disciplinas';
+import Sobre from './Sobre';
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
+      <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> <Link to='/home'>HOME</Link></p>
+        <p><Link to='/disciplinas'>Disciplinas oferecidas</Link></p>
+        <p><Link to='/sobre/'>Sobre</Link></p>
       </header>
-    </div>
+
+      <main>
+        <Switch>
+          <Route path='/home' component= {Home}/>
+          <Route path='/disciplinas' component= {Disciplinas}/>
+          <Route path ='/sobre/:id' component= {Sobre} />
+        </Switch>
+      </main>
+      </>
+
   );
 }
 
