e55fc95c461376005b1c3590fe48b7d72cebb745
Vladfarias <vladfarias@gmail.com>
2023-04-17 21:41:06 -0300

    fix: fixed function name

--- src/Disciplinas.js
+++ src/Disciplinas.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './index.css';
 
-export default function Usuario() {
+export default function Disciplinas() {
     return(
         <div>
             <h1>Disciplinas oferecidas:</h1>
